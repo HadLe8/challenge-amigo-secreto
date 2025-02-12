b56f16858059b1473c0874da836687179816162c
unknown <hadara_elizabeth_@hotmail.com>
2025-02-12 10:30:15 -0600

    Configura funcion agregar amigo

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos=[];
 
+
+
+
 function campoNombre(){ // escribir nombre
-    let entradaUsuario = document.getElementById('amigo').value;
-    console.log(entradaUsuario);
+    
+    
 }
 
-function agregarAmigo(nombre){
-    amigos.push(nombre);
+function agregarAmigo(){
+    let entradaUsuario = document.getElementById('amigo').value;
+    asignarTextoElemento('h2',entradaUsuario);
+    amigos.push(entradaUsuario);
+    return;
+    
 }
+console.log(amigos);
 
 function sortearAmigo(){
 
 }
 
 function limpiarCampo(){
+    document.querySelector('amigo').value = '';
+}
 
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
