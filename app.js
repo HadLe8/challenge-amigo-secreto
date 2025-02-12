e7b29b0b624992497c57cb74c461e75b23213c15
unknown <hadara_elizabeth_@hotmail.com>
2025-02-12 10:00:36 -0600

    Configurar funciones

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos=[];
 
-function añadirNombre(){
-    
+function campoNombre(){ // escribir nombre
+    let entradaUsuario = document.getElementById('amigo').value;
+    console.log(entradaUsuario);
 }
 
-function agregarAmigo(){
-
+function agregarAmigo(nombre){
+    amigos.push(nombre);
 }
 
 function sortearAmigo(){
