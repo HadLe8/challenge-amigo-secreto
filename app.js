dd8d12005b96aa60197d7cda781e0b3181135674
unknown <hadara_elizabeth_@hotmail.com>
2025-02-15 09:14:52 -0600

    Se actualiza funcion sortear amigos, se genera copia de matriz original

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos=[];
+let amigosCopia= amigos.slice();
 
 function agregarAmigo(){
     let entradaUsuario = document.getElementById('amigo').value;
     asignarTextoElemento('#listaAmigos',entradaUsuario); //
     amigos.push(entradaUsuario);
     limpiarCampo();
+    console.log(amigos);
 }
 
 function sortearAmigo(){
-    sorteo= amigos.sort(() => Math.random() - 0.5); 
-    asignarTextoElemento('#resultado',nombreSorteado);
+    nombreSorteado= amigosCopia.sort(() => Math.random() - 0.5); 
+    asignarTextoElemento('#resultado',nombreSorteado[0]);
+    console.log(amigosCopia);
 }
 
 function limpiarCampo(){
