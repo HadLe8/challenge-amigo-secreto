13ebda9dd6a0f5af135699cf10b0e0bc7518aa7a
unknown <hadara_elizabeth_@hotmail.com>
2025-02-15 09:48:45 -0600

    Ajuste de funciones

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos=[];
-let amigosCopia= amigos.slice();
+let amigosCopia=[];
 
 function agregarAmigo(){
     let entradaUsuario = document.getElementById('amigo').value;
-    asignarTextoElemento('#listaAmigos',entradaUsuario); //
-    amigos.push(entradaUsuario);
-    limpiarCampo();
-    console.log(amigos);
+    asignarTextoElemento('#listaAmigos',entradaUsuario); // usar la función para imprimir en el elemento
+    amigos.push(entradaUsuario); //añadir elementos al array
+    limpiarCampo('#amigo');
 }
 
 function sortearAmigo(){
-    nombreSorteado= amigosCopia.sort(() => Math.random() - 0.5); 
+    limpiarCampo('#listaAmigos');
+    amigosCopia= amigos.slice(); // copiar matriz
+    //compara si la posición del arreglo es positivo o negativo y lo ordena
+    nombreSorteado= amigosCopia.sort(() => Math.random() - 0.5);  //Genera un numero aleatorio entre -0.5 y 0.5(equilibrio entre positivos y negativos)
     asignarTextoElemento('#resultado',nombreSorteado[0]);
-    console.log(amigosCopia);
 }
 
-function limpiarCampo(){
-    document.querySelector('#amigo').value = '';
+function limpiarCampo(id){
+    document.querySelector(id).value = '';
 }
 
 function asignarTextoElemento(elemento, texto) {
