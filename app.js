baef7cf3b58b4b5414627e08837d52889420671e
unknown <hadara_elizabeth_@hotmail.com>
2025-02-17 09:45:41 -0600

    Antes de iniciar con la lógica de botones

--- app.js
+++ app.js
@@ -1,49 +1,44 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos=[];
 let amigosCopia=[];
 
-function limpiarCampo(id){
-    document.querySelector(id).value = '';
-    return;
-}
 
 function agregarAmigo(){
     let entradaUsuario = document.getElementById('amigo').value;
-    if (entradaUsuario.trim() !== ''){
+    if (entradaUsuario == ''){
         alert("Ingresa un nombre válido");
     }else{
-        asignarTextoElemento('#listaAmigos',entradaUsuario); // usar la función para imprimir en el elemento
-        amigos.push(entradaUsuario); //añadir elementos al array
-    }
-    limpiarCampo('#amigo');
-    return;
-}
-/*
-function agregarAmigo() {
-    let entradaUsuario = document.getElementById('amigo').value;
-    if (entradaUsuario.trim() !== '') { // Verifica que el campo no esté vacío
-        amigos.push(entradaUsuario); //añadir elementos al array
-        let listaAmigosHTML = '';
-        for (let i = 0; i < amigos.length; i++) {
-            listaAmigosHTML += `<div>${amigos[i]}</div>`;
-        }
-        asignarTextoElemento('#listaAmigos', listaAmigosHTML);
+        // usar la función para imprimir en el elemento
+        asignarTextoElemento('#listaAmigos',entradaUsuario); 
+        //añadir elementos al array
+        amigos.push(entradaUsuario);
+        //limpiar entrada usuario
         limpiarCampo('#amigo');
     }
+    return;
 }
-*/
 
 function sortearAmigo(){
-    asignarTextoElemento('#listaAmigos'," "); // Limpia el contenido de #listaAmigos
-    amigosCopia = amigos.slice(); // Copia el array de amigos
-    nombreSorteado = amigosCopia.sort(() => Math.random() - 0.5); //sortea posiciones aleatoriamente
+    // Limpia el contenido de #listaAmigos
+    asignarTextoElemento('#listaAmigos'," "); 
+     // Copia el array de amigos
+    amigosCopia = amigos.slice();
+    //Sortea posiciones aleatoriamente
+    nombreSorteado = amigosCopia.sort(() => Math.random() - 0.5); 
+    //Muestra la primera posición del array sorteado
     asignarTextoElemento('#resultado', nombreSorteado[0]);
     return;
     
 }
 
 function asignarTextoElemento(elemento, texto) {
+    //asignar texto a un elemento
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
+}
+
+function limpiarCampo(id){
+    //Darle valor vacio a un elemento
+    document.querySelector(id).value = '';
     return;
 }
